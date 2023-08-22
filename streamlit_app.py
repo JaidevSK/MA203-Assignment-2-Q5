9c959a3b73660562d41687cac4ae1ec6c60f9b51
JaidevSK <134712750+JaidevSK@users.noreply.github.com>
2023-08-22 20:04:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.write(" Made By: Jaidev S. K.")
 
 import math
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
 Re = st.slider('#### Select the value of Re', 2500, 1000000, 15000)
@@ -44,13 +44,21 @@ y = []
 for i in x:
     y.append(f(i))
 y=np.array(y)
-fig = plt.figure()
-plt.plot(x,y)
-plt.xlabel("Value of f")
-plt.ylabel("Value of f(f)")
-plt.title("Plot of f vs f(f) for verification")
-plt.grid()
-st.pyplot(fig)
+# fig = plt.figure()
+# plt.plot(x,y)
+# plt.xlabel("Value of f")
+# plt.ylabel("Value of f(f)")
+# plt.title("Plot of f vs f(f) for verification")
+# plt.grid()
+# st.pyplot(fig)
+
+import plotly.express as px
+import pandas as pd
+df = pd.DataFrame(dict(
+    x = x,
+    y = y))
+fig = px.line(df, x="Value of f", y="Value of f(f)", title="Plot of f vs f(f) for verification") 
+st.plotly_chart(fig)
 
 st.write("I have used the fixed point iteration method")
 st.write("It is an open numerical method to find the approximate root of the equation. I have set the threshold of approximate error as 10^-8 and the value of Re is taken as input. The output is the value of f, that is the 'Fanning friction f' and a plot corresponding to the value of fn(f) and f. The expression of fn(f) is:  1/math.sqrt(f) - 4\*math.log10(Re*math.sqrt(f)) + 0.4")
